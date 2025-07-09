8f005aa49a2eb82454bdbe50e1e8a52f2a9a1d0e
camillaquiros2 <cquirost@ucenfotec.ac.cr>
2025-07-09 12:59:51 -0600

    Correccion de Navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  imports: [NgIf, RouterLink]
 })
 export class NavComponent {
   isLoggedIn = false;
