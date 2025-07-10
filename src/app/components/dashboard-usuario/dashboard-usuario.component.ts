edfafd8cf07e1fe2766ac8e0418bcf7b247c4485
lbarrantesm <lbarrantesm@ucenfotec.ac.cr>
2025-07-09 21:44:50 -0600

    Arregle los enlaces de las fotos rotas , cambie el codigo de tailwind a css porque es mas facil, hice cambios en componentes relacionados a dashboardUsuario

--- src/app/components/dashboard-usuario/dashboard-usuario.component.ts
+++ src/app/components/dashboard-usuario/dashboard-usuario.component.ts
@@ -1,14 +1,14 @@
 import { Component } from "@angular/core";
 import { trigger, transition, style, animate } from "@angular/animations";
-import { DailyTipComponent } from '../daily-tip/daily-tip.component';
-
+import { DailyTipComponent } from "../daily-tip/daily-tip.component";
+import { CardInfoUserComponent } from "../card-info-user/card-info-user.component";
 
 @Component({
   selector: "app-dashboard-usuario",
-  imports: [ DailyTipComponent],
+  standalone: true,
+  imports: [DailyTipComponent, CardInfoUserComponent],
   templateUrl: "./dashboard-usuario.component.html",
   styleUrls: ["./dashboard-usuario.component.scss"],
-  standalone: true,
   animations: [
     trigger("fadeInOut", [
       transition(":enter", [
