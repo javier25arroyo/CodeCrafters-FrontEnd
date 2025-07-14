747c4d11589ec584d3fb727efc5648ed661af452
lbarrantesm <lbarrantesm@ucenfotec.ac.cr>
2025-07-14 14:04:40 -0600

    Revisando con javi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,92 +1,99 @@
-import { Routes } from "@angular/router";
-import { AppLayoutComponent } from "./components/app-layout/app-layout.component";
-import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
-import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
-import { FondoBonitoComponent } from "./fondo-bonito/fondo-bonito.component";
-import { AuthGuard } from "./guards/auth.guard";
-import { GuestGuard } from "./guards/guest.guard";
-import { IRoleType } from "./interfaces";
-import { AccessDeniedComponent } from "./pages/access-denied/access-denied.component";
-import { LoginComponent } from "./pages/auth/login/login.component";
-import { SigUpComponent } from "./pages/auth/sign-up/signup.component";
-import { DashboardAdminComponent } from "./pages/dashboard-admin/dashboard-admin.component";
-import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
-import { ProfileComponent } from "./pages/profile/profile.component";
 
+import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { FondoBonitoComponent } from './fondo-bonito/fondo-bonito.component';
+import { AuthGuard } from './guards/auth.guard';
+import { GuestGuard } from './guards/guest.guard';
+import { IRoleType } from './interfaces';
+import { AccessDeniedComponent } from './pages/access-denied/access-denied.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { SigUpComponent } from './pages/auth/sign-up/signup.component';
+import { DashboardAdminComponent } from './pages/dashboard-admin/dashboard-admin.component';
+import { DashboardUsuarioComponent } from './components/dashboard-usuario/dashboard-usuario.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GameGalleryComponent } from './game-gallery/game-gallery.component';
 
 export const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: LandingPageComponent,
   },
   {
-    path: "login",
+    path: 'login',
     component: LoginComponent,
     canActivate: [GuestGuard],
   },
   {
-    path: "game-gallery",
-    loadComponent: () =>
-      import("./game-gallery/game-gallery.component").then(
-        (m) => m.GameGalleryComponent
-      ),
-  },
-  {
-    path: "dashboard-user",
-    loadComponent: () =>
-      import("./components/dashboard-usuario/dashboard-usuario.component").then(
-        (m) => m.DashboardUsuarioComponent
-      ),
-  },
-  {
-    path: "dashboard-admin",
-    component: DashboardAdminComponent,
+    path: 'game-gallery',
+    component: GameGalleryComponent,
+    canActivate: [AuthGuard],
     data: {
-      name: "dashboard admin",
+      authorities: [IRoleType.user],
+    },
+  },
+  {
+    path: 'dashboard-user',
+    component: DashboardUsuarioComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'dashboard-admin',
+    component: DashboardAdminComponent,
+    canActivate: [AuthGuard],
+    data: {
+      name: 'dashboard admin',
       showInSidebar: true,
     },
   },
   {
-    path: "signup",
+    path: 'signup',
     component: SigUpComponent,
     canActivate: [GuestGuard],
   },
   {
-    path: "forgot-password",
+    path: 'forgot-password',
     component: ForgotPasswordComponent,
     canActivate: [GuestGuard],
   },
   {
-    path: "reset-password",
+    path: 'reset-password',
     component: ResetPasswordComponent,
   },
   {
-    path: "access-denied",
+    path: 'access-denied',
     component: AccessDeniedComponent,
   },
   {
-    path: "fondo-bonito",
+    path: 'fondo-bonito',
     component: FondoBonitoComponent,
   },
   {
-    path: "app",
+    path: 'app',
     component: AppLayoutComponent,
     canActivate: [AuthGuard],
     children: [
       {
-        path: "app",
-        redirectTo: "users",
-        pathMatch: "full",
+        path: 'users',
+        component: ProfileComponent,
+        data: {
+          authorities: [IRoleType.admin, IRoleType.superAdmin],
+          name: 'Users',
+          showInSidebar: true,
+        },
       },
       {
-        path: "profile",
+        path: 'profile',
         component: ProfileComponent,
         data: {
           authorities: [IRoleType.admin, IRoleType.superAdmin, IRoleType.user],
-          name: "profile",
+          name: 'profile',
           showInSidebar: false,
         },
       },
     ],
   },
 ];
+
