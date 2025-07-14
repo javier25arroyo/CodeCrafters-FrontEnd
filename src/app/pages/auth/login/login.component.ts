1f20ec7045e83797cf2cff7a949707d0cb21f9ce
leo0994 <80069598+leo0994@users.noreply.github.com>
2025-07-14 12:03:57 -0600

    Update login.component.ts
    correcion de commilas dobles a simples

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,26 +1,26 @@
-import { CommonModule } from "@angular/common";
-import { Component, ViewChild } from "@angular/core";
-import { FormsModule, NgModel } from "@angular/forms";
-import { Router, RouterLink } from "@angular/router";
-import { AuthService } from "../../../services/auth.service";
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
-  selector: "app-login",
+  selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterLink],
-  templateUrl: "./login.component.html",
-  styleUrl: "./login.component.scss",
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   public loginError!: string;
-  public passwordFieldType: string = "password";
+  public passwordFieldType: string = 'password';
 
-  @ViewChild("email") emailModel!: NgModel;
-  @ViewChild("password") passwordModel!: NgModel;
+  @ViewChild('email') emailModel!: NgModel;
+  @ViewChild('password') passwordModel!: NgModel;
 
   public loginForm: { email: string; password: string } = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   };
 
   constructor(
@@ -30,7 +30,7 @@ export class LoginComponent {
 
   public togglePasswordVisibility(): void {
     this.passwordFieldType =
-      this.passwordFieldType === "password" ? "text" : "password";
+      this.passwordFieldType === 'password' ? 'text' : 'password';
   }
 
   public handleLogin(event: Event) {
@@ -46,12 +46,12 @@ export class LoginComponent {
         next: () => {
           // Usa hasRole para verificar si el usuario es admin o superAdmin
           if (
-            this.authService.hasRole("ADMIN") ||
+            this.authService.hasRole('ADMIN') ||
             this.authService.isSuperAdmin()
           ) {
-            this.router.navigateByUrl("/dashboard-admin");
+            this.router.navigateByUrl('/dashboard-admin');
           } else {
-            this.router.navigateByUrl("/dashboard-user");
+            this.router.navigateByUrl('/dashboard-user');
           }
         },
         error: (err: any) => (this.loginError = err.error.description),
