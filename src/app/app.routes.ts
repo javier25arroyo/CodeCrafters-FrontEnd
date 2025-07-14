41a377cd8526216abe721981cce441bbbdd97e39
lbarrantesm <lbarrantesm@ucenfotec.ac.cr>
2025-07-14 10:49:22 -0600

    Eliminando archivos que no necesitamos (trabajo en grupo)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,14 @@ import { UsersComponent } from './pages/users/users.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AccessDeniedComponent } from './pages/access-denied/access-denied.component';
 import { AdminRoleGuard } from './guards/admin-role.guard';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GuestGuard } from './guards/guest.guard';
 import { IRoleType } from './interfaces';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { GamesComponent } from './pages/games/games.component';
-import { OrdersComponent } from './pages/orders/orders.component';
-import { PreferenceListPageComponent } from './pages/preferenceList/preference-list.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { FondoBonitoComponent } from './fondo-bonito/fondo-bonito.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component'; // agrega esta importación
+import { LandingPageComponent } from './pages/landing-page/landing-page.component'; 
+import { DashboardAdminComponent } from './pages/dashboard-admin/dashboard-admin.component';
 
 export const routes: Routes = [
   {
@@ -41,6 +38,14 @@ export const routes: Routes = [
       import('./components/dashboard-usuario/dashboard-usuario.component').then(
         (m) => m.DashboardUsuarioComponent
       ),
+  },
+  {
+    path: 'dashboard-admin',
+    component: DashboardAdminComponent,
+    data: {
+      name: 'dashboard admin',
+      showInSidebar: true,
+    },
   },
   {
     path: 'signup',
@@ -84,15 +89,7 @@ export const routes: Routes = [
           showInSidebar: true,
         },
       },
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-        data: {
-          authorities: [IRoleType.admin, IRoleType.superAdmin, IRoleType.user],
-          name: 'Dashboard',
-          showInSidebar: true,
-        },
-      },
+      
       {
         path: 'profile',
         component: ProfileComponent,
@@ -102,33 +99,6 @@ export const routes: Routes = [
           showInSidebar: false,
         },
       },
-      {
-        path: 'games',
-        component: GamesComponent,
-        data: {
-          authorities: [IRoleType.admin, IRoleType.superAdmin, IRoleType.user],
-          name: 'games',
-          showInSidebar: true,
-        },
-      },
-      {
-        path: 'orders',
-        component: OrdersComponent,
-        data: {
-          authorities: [IRoleType.admin, IRoleType.superAdmin, IRoleType.user],
-          name: 'orders',
-          showInSidebar: true,
-        },
-      },
-      {
-        path: 'preference-list',
-        component: PreferenceListPageComponent,
-        data: {
-          authorities: [IRoleType.admin, IRoleType.superAdmin, IRoleType.user],
-          name: 'preference list',
-          showInSidebar: true,
-        },
-      },
     ],
   },
-]
+];
