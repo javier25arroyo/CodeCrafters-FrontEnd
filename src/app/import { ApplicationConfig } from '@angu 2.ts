66abbfbb626d926669cb98c808131ed5d4b28be1
camillaquiros2 <cquirost@ucenfotec.ac.cr>
2025-07-22 17:26:15 -0600

    Cambios

---
+++ src/app/import { ApplicationConfig } from '@angu 2.ts
@@ -0,0 +1,41 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { baseUrlInterceptor } from './interceptors/base-url.interceptor';
+import { accessTokenInterceptor } from './interceptors/access-token.interceptor';
+import { handleErrorsInterceptor } from './interceptors/handle-errors.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideHttpClient(
+      withInterceptors([
+        baseUrlInterceptor,
+        accessTokenInterceptor,
+        //handleErrorsInterceptor
+      ])
+    ), 
+    provideAnimationsAsync(),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('450242025121-l9p7v1jvg34qric7g4r1rgletiupk0pa.apps.googleusercontent.com', {
+              oneTapEnabled: false,
+              scopes: 'openid profile email',
+            })
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ]
+};
