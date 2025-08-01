e6afbf127b1f3524a11d628e6f60f097ba99d4b6
Luisferha <luisferha04@gmail.com>
2025-08-01 01:03:18 -0600

    update email and melody memory
    todo funcional

--- src/app/pages/melody-memory/melody-memory.component.ts
+++ src/app/pages/melody-memory/melody-memory.component.ts
@@ -172,7 +172,7 @@ export class MelodyMemoryComponent implements OnInit {
       case 'easy': return 1;
       case 'medium': return 3;
       case 'hard': return 5;
-      default: return 2;
+      default: return 1;
     }
   }
 }
